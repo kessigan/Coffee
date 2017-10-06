5c7acb708cf5c395c20b7b2f1b5c4926980044b1
Kessigan Subramanian <kessigan.subramanian@gmail.com>
2017-10-06 14:07:25 +0200

    hello

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
-var router = express.Router();
-var path = __dirname + '/views/';
 
-router.use(function (req,res,next) {
-  console.log("/" + req.method);
-  next();
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
-});
-
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
-});
-
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
-});
-
-app.use("/",router);
-
-
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
