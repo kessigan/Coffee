74c4b88926f37a6bfd94cab6c217a6bf5ffde9b7
Kessigan Subramanian <kessigan.subramanian@gmail.com>
2017-10-06 16:51:02 +0200

    added pages

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/measurements', function(request, response) {
+  response.render('pages/measurements');
+});
+
+app.get('/predictions', function(request, response) {
+  response.render('pages/predictions');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
+
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
